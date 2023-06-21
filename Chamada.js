d69340337d9881fbbaede421df960b53a26bbe41
Organnact-SSI <sistemas.organnact@gmail.com>
2023-06-21 09:58:04 -0300

    Commit Codigo App

---
+++ Chamada.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, FlatList, Text, ListItem } from 'react-native';
+
+
+const url = "http://10.200.23.27:5002/Api/Producao/Apontamento"
+
+let payload = []
+
+// payload = {
+//   Company: "1",
+//   Nr_ini: "1",
+//   Nr_fin: "9999999",
+//   Dt_ini: "2023-06-01T03:00:00.000",
+//   Dt_fin: "2023-06-14T03:00:00.000"}
+
+
+export default class Chamada extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { dataSource: [] };
+  }
+async componentDidMount() {
+  try {
+    const response = await fetch(url, {
+         method: 'POST',
+         body: JSON.stringify(payload),
+         headers: {
+          'Content-type': 'application/json'},})
+    const responseJson = await response.json();
+    this.setState({dataSource : responseJson});
+    return responseJson;
+}
+catch (error) {
+    console.error("error is ",error);
+    return { err: error };
+}
+    }
+   
+    renderItem = () => (
+      this.state.dataSource.map(data => <Text>{data.descricao}</Text>
+      )
+    )
+    
+  render() {
+    console.log('TESTE')
+    return (
+      this.state.dataSource
+    )
+  }  }
